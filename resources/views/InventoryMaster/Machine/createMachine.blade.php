e566e2ef81917bbff8a7bf9dcf6a3a03b135a032
Muhammad Haziq <muhammadhaziq.me@gmail.com>
2026-03-12 04:08:43 -0700

    set.

--- resources/views/InventoryMaster/Machine/createMachine.blade.php
+++ resources/views/InventoryMaster/Machine/createMachine.blade.php
@@ -51,7 +51,7 @@
 
                                                             <div class="col-md-4">
                                                                 <div class="form-group">
-                                                                    <label for="name">Mixture Machine Name</label>
+                                                                    <label for="name">Machine Name</label>
                                                                     <input type="text" name="name" id="name"
                                                                         class="form-control" required>
                                                                 </div>
